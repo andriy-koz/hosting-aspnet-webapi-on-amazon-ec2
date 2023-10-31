8b672421b5d868b294d120b2cef8b652ae3df9c9
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-10-31 02:23:29 -0300

    changes

---
+++ src/ProductCRUD.API/ProductDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCRUD.API.Models;
+
+public class ProductDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+    {
+    }
+}
